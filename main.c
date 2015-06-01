37b3745982ffcf86d6399ee7d70b83e70cc7f6a1
ttakaluo <teemu.takaluoma@gmail.com>
2015-06-01 14:51:17 +0300

    v1.0

--- main.c
+++ main.c
@@ -6,11 +6,18 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>  //fork
 
 void error(char *msg)
 {
     perror(msg);
     exit(1);
+}
+
+int talk_to_client(int newsockfd){
+
+	//this is the child-process handling incoming communication
+
 }
 
 int main(int argc, char *argv[]){
@@ -41,31 +48,54 @@ int main(int argc, char *argv[]){
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;    //accept all interfaces
 	serv_addr.sin_port = htons(portno);		 //convert network byte order to host byte order
+
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR on binding");
 
 	// wait for connect
 	struct sockaddr_in cli_addr;
 	int clilen;
+
+	while(1){
+	
      	listen(sockfd,5);
 	clilen = sizeof(cli_addr);
-
-	// when connected, fork
 	int newsockfd;
 	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 	if (newsockfd < 0)
 		error("ERROR on accept");
 
+	// when connected, fork
+
+	pid_t childPid;
+	childPid = fork();
+	
+	if (childPid == 0) {
+	
+		char buffer[256];
+		int n;
+		memset(buffer,0,256);
+
+		do{
+			n = read(newsockfd,buffer,255);
+			if (n < 0) {
+				error("ERROR reading from socket");
+				return 1;
+			}
+		}
+		while (n != 0) ;
+
+		printf("Got this message: %s\n", buffer);
+	
+		return 0;
+	}
+	else if (childPid == -1) {
+		error("Problem while forking");
+		return 1;
+	}
+	} 
 	// past to child return from accept
 	// loop to wait for new connections
 
-	char buffer[256];
-	int n;
-	memset(buffer,0,256);
-	n = read(newsockfd,buffer,255);
-	if (n < 0) error("ERROR reading from socket");
-	printf("Here is the message: %s\n",buffer);
-	n = write(newsockfd,"I got your message",18);
-	if (n < 0) error("ERROR writing to socket");
 	return 0; 
 }
